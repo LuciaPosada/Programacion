2ae2690d56f6ea542f372da7eb649b0f86d6f450
Lucia <lposadagonzalez@danielcastelao.org>
2023-10-02 10:17:39 +0200

    añadido boletin2_1

---
+++ Boletin02/boletin2_1/src/Boletin2_1.java
@@ -0,0 +1,17 @@
+
+package boletin2_1;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin2_1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float base = 3,altura = 4,area;
+        area = base*altura/2;
+        System.out.println("area = " +area);
+    }
+    
+}
