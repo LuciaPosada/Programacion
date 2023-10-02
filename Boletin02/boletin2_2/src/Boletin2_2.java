c87ae0b26055bdbd9e27c54382f3bd039eafa71d
Lucia <lposadagonzalez@danielcastelao.org>
2023-10-02 10:20:13 +0200

    añadido boletin2_2

---
+++ Boletin02/boletin2_2/src/Boletin2_2.java
@@ -0,0 +1,26 @@
+
+package boletin2_2;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin2_2 {
+
+    public static void main(String[] args) {
+        
+        float lado = 3,area;
+        
+        //Operaciones
+        area = lado*lado;
+        
+        //Modo 2:
+        //area = (float) Math.pow(lado, 2);
+        
+        System.out.println("area = "+area);
+        
+        //Sin declara la variable area
+        //System,out.println ("area = "+ lado*lado)
+    }
+    
+}
