ae2f410154c1dca9950a637b3d325c81e4db1b6b
Lucia <lposadagonzalez@danielcastelao.org>
2023-10-30 08:42:53 +0100

    añadido boletin 9_1

---
+++ Boletin09/boletin9_1/src/Boletin9_1.java
@@ -0,0 +1,18 @@
+
+package boletin9_1;
+
+public class Boletin9_1 {
+
+    public static void main(String[] args) {
+
+        /*
+        calcular a cantidade de numeros positivos,negativos e ceros que hay nun gripo de 10 numeros enteiros
+        */
+        
+        Numeros obx =new Numeros();
+        
+        obx.contarNumeros();
+
+    }
+    
+}
---
+++ Boletin09/boletin9_1/src/LerDatos.java
@@ -0,0 +1,16 @@
+
+package boletin9_1;
+
+import javax.swing.JOptionPane;
+
+public class LerDatos {
+    
+    public static int lerEnteiro(String mensaxe){
+        int resposta = Integer.parseInt(JOptionPane.showInputDialog(mensaxe));
+        return resposta;
+        
+        //otro modo 
+       //return Integer.parseInt(JOptionPane.showInputDialog(mensaxe));
+    }
+    
+}
