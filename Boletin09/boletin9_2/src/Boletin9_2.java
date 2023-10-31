7a6993ccbd338e43ccd580739ca8fa747dc0b194
Lucia <lposadagonzalez@danielcastelao.org>
2023-10-31 13:56:01 +0100

    añadido boletin 9_2

---
+++ Boletin09/boletin9_2/src/Boletin9_2.java
@@ -0,0 +1,16 @@
+
+package boletin9_2;
+
+
+public class Boletin9_2 {
+
+
+    public static void main(String[] args) {
+
+        Acumulador obx =new Acumulador();
+        
+        obx.acumuladorSumaProducto();
+           
+    }
+    
+}
