240661c76463861c7130cb804d26527b7ebf42e0
Lucia <lposadagonzalez@danielcastelao.org>
2023-11-17 08:50:55 +0100

    añadido _3,_4,_5 al boletin 9

---
+++ Boletin09/boletin9_3/src/Boletin9_3.java
@@ -0,0 +1,16 @@
+
+package boletin9_3;
+
+
+public class Boletin9_3 {
+
+
+    public static void main(String[] args) {
+
+        Rectangulo obxR = new Rectangulo();
+        
+        obxR.calcularArea();
+        
+    }
+    
+}
