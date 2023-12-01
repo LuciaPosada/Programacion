5b7d22e51c721fb350bade56cee31b846c9419c0
Lucia <lposadagonzalez@danielcastelao.org>
2023-12-01 12:45:00 +0100

    actualizado 18_1

--- Boletin18/Boletin18_1/src/Boletin18_1.java
+++ Boletin18/Boletin18_1/src/Boletin18_1.java
@@ -10,16 +10,39 @@ public class Boletin18_1 {
         int continuar = 2;
 
         do {
-
+            
             int duracion = Integer.parseInt(JOptionPane.showInputDialog("Tecle en minutos la duracion de la llamada"));
+            
+            if(duracion > 0){
+                
+                int dia = Integer.parseInt(JOptionPane.showInputDialog("Tecle el dia de la semana: \n1- Lunes \n2- Martes \n3- Miercoles \n4- Jueves \n5- Viernes \n6- Sabado \n7-Domingo "));
+                
+                if(dia <= 7&&dia >= 1){
+                    
+                    int parteDia = Integer.parseInt(JOptionPane.showInputDialog("Tecle la parte del dia: \n1- Mañana // 2 -Tarde "));
 
-            int dia = Integer.parseInt(JOptionPane.showInputDialog("Tecle el dia de la semana: \n1- Lunes \n2- Martes \n3- Miercoles \n4- Jueves \n5- Viernes \n6- Sabado \n7-Domingo "));
-
-            int parteDia = Integer.parseInt(JOptionPane.showInputDialog("Tecle la parte del dia: \n1- Mañana // 2 -Tarde "));
-
-            double total = Calculos.calcularCoste(duracion, dia, parteDia);
-
-            JOptionPane.showMessageDialog(null, "Precio de la llamada: " + total + " €");
+                    if(parteDia <= 2&&parteDia >= 1){
+                        
+                        double total = Calculos.calcularCoste(duracion, dia, parteDia);
+                        
+                        JOptionPane.showMessageDialog(null, "Precio de la llamada: " + total + " €");
+                        
+                    }else{
+                        
+                        JOptionPane.showMessageDialog(null, "Parte del dia invalida");
+                    }
+                    
+                }else{
+                    
+                    JOptionPane.showMessageDialog(null, "Dia invalido");
+                    
+                }
+                
+            }else{
+                
+                JOptionPane.showMessageDialog(null, "Duracion invalida");
+                
+            }
             
             continuar = JOptionPane.showConfirmDialog(null,"Desea cerrar el programa?");
 
--- Boletin18/Boletin18_1/src/Calculos.java
+++ Boletin18/Boletin18_1/src/Calculos.java
@@ -8,13 +8,13 @@ public class Calculos {
         double coste = 0;
 
         if (duracion <= 5) {
-            coste = 1.0;
+            coste = duracion*1.0;
         } else if (duracion <= 8) {
-            coste = 1.0 + (duracion - 5) * 0.8;
+            coste = (1.0*(duracion - 3))+((duracion-5)* 0.8);
         } else if (duracion <= 10) {
-            coste = 1.0 + 3 * 0.8 + (duracion - 8) * 0.7;
+            coste = (5.0+2.4)+((duracion-8)* 0.7);
         } else {
-            coste = 1.0 + 3 * 0.8 + 2 * 0.7 + (duracion - 10) * 0.5;
+            coste = (5.0+2.4+1.4) + ((duracion - 10) * 0.5);
         }
 
 
